dc65b75e95a4bae45dafaf0709ceca3eff26d6f5
Maxy P <maxy.dp@abv.bg>
2021-03-16 01:55:18 +0200

    added error session message

--- src/sessions.php
+++ src/sessions.php
@@ -1,9 +1,17 @@
 <?php 
 
-function success_message_registration() {
+function success_message() {
   if (isset($_SESSION['success_message'])) {
     $output = "<div class=\"alert alert-success\">{$_SESSION['success_message']}</div>";
     $_SESSION['success_message'] = null;
+    return $output;
+  }
+}
+
+function error_message() {
+  if (isset($_SESSION['error_message'])) {
+    $output = "<div class=\"alert alert-danger\">{$_SESSION['error_message']}</div>";
+    $_SESSION['error_message'] = null;
     return $output;
   }
 }
