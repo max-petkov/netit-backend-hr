bbb7d31b2f2283e29a6a512e9e53bcfa83ae3cf3
Maxy P <maxy.dp@abv.bg>
2021-03-23 03:46:53 +0200

    404 page

---
+++ 404-page.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/custom.css">
+  <title>Monster HR | 404 error</title>
+</head>
+<body>
+  <section class="container d-flex align-items-center justify-content-center vh-100">
+    <div class="d-flex align-items-center flex-column flex-md-row"> 
+        <img src="assets/img/404-img.png" class="img-fluid" alt="">
+        <p class="display-3 ms-md-3">The <span style="color:#614aa8;">page</span> you were looking for does not exist...</p>  
+    </div>
+  </section>
+</body>
+</html>
