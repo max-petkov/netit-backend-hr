9c6e73d29b049270d426c59b4c02984649b39ac8
Maxy P <maxy.dp@abv.bg>
2021-03-25 03:51:34 +0200

    new logout location

---
+++ src/logout.php
@@ -0,0 +1,12 @@
+<?php 
+include 'database.php';
+include_once 'functions.php';
+include_once 'sessions.php';
+
+// The session value from id will be removed
+$_SESSION['employee_id'] = null;
+$_SESSION['success_message'] = "Successfully logged out!";
+redirect_to('../login.php');
+session_destroy();
+
+?>
