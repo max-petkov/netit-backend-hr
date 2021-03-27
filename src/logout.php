046ce7c2a67bc46446c12668324645384624591d
Maxy P <maxy.dp@abv.bg>
2021-03-27 04:46:33 +0200

    adding company session value

--- src/logout.php
+++ src/logout.php
@@ -4,7 +4,8 @@ include_once 'functions.php';
 include_once 'sessions.php';
 
 // The session value from id will be removed
-$_SESSION['employee_id'] = null;
+$_SESSION['employee_id']     = null;
+$_SESSION['company_id']      = null;
 $_SESSION['success_message'] = "Successfully logged out!";
 redirect_to('../login.php');
 session_destroy();
