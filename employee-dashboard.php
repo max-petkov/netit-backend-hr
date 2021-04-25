736de80a9f6920a887daf24e6285cddb7e1de349
Maxy P <maxy.dp@abv.bg>
2021-04-25 20:03:47 +0300

    refactoring

--- employee-dashboard.php
+++ employee-dashboard.php
@@ -11,6 +11,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
   <link rel="stylesheet" href="assets/css/bootstrap.css">
   <link rel="stylesheet" href="assets/css/custom.css">
   <title>👾Monster HR | Dashboard</title>
@@ -51,9 +52,10 @@
                 </li>
                 <div class="dropdown-divider"></div>
                 <li class="dropdown-item xsm-text-class d-flex align-items-center">
-                  <svg class="me-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                <i class="bi bi-person"></i>
+                  <!-- <svg class="me-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                  </svg>
+                  </svg> -->
                   <span id="employee_first_name" class="me-1">
                     <?php echo $profile->name; ?>
                   </span>
